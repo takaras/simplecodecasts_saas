d71bb56bf780e03c372ff0afd49d40bc746e827e
Dimitrios Gkessos <ges.dim@gmail.com>
2018-02-11 18:36:01 +0000

    Fixed routing problem in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationController < Devise::RegistrationController
+class Users::RegistrationsController < Devise::RegistrationsController
    
    def create
        super do |resource|
